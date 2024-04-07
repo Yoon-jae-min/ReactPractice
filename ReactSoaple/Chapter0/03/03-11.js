f906c573fa11ee6ececdf1aa22a5dffd0b72d0cb
Yoon-Jae-Min <jmyoon1994@naver.com>
2024-04-07 20:12:04 +0900

    20240407-home

---
+++ ReactSoaple/Chapter0/03/03-11.js
@@ -0,0 +1,6 @@
+let a = true;
+let b = false;
+
+console.log(a ? 1 : 2);
+
+console.log(b ? 3 : 2);
---
+++ ReactSoaple/Chapter0/03/03-9.js
@@ -0,0 +1,10 @@
+let a = 1;
+let b = '1';
+
+console.log(a == b);
+
+console.log(a != b);
+
+console.log(a === b);
+
+console.log(a !== b);
