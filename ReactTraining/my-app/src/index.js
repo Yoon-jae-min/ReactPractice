a896e1d97dd1fb2fed5b2ba2860d0b55e0a12368
Yoon-jae-min <jmyoon1994@naver.com>
2024-05-01 18:03:44 +0900

    20240501

--- ReactTraining/my-app/src/index.js
+++ ReactTraining/my-app/src/index.js
@@ -6,8 +6,10 @@ import Hello from './ch03/Hello';
 import Library from './ch03/Library';
 import Clock from './ch04/Clock';
 import CommentList from './ch05/CommentList';
+import NotificationList from './ch06/NotificationList';
+import Counter from './ch07/Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<CommentList/>)
+root.render(<NotificationList/>)
 
 reportWebVitals();
